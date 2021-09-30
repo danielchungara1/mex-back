1cc90e907ae3043c8a6b4814e4e26d9a44dbc4c4
DanielChungara <danielchungara1@gmail.com>
2021-09-30 20:35:20 -0300

    Adding mongo db.

--- src/products/ProductRepository.ts
+++ src/products/ProductRepository.ts
@@ -1,114 +1,31 @@
 import { Service } from "typedi";
 import Product from "./Product";
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+
+const ProductSchema = {
+  _id: Number,
+  description: String,
+  price: Number,
+  stock: Number,
+  imageUrlSm: String,
+  imageUrlMd: String,
+  imageUrlLg: String,
+}
+
+const ProductModel = mongoose.model('Product', ProductSchema);
+
 @Service()
 class ProductRepository {
 
-  private readonly products: Product[] = [
-    {
-      _id: 1,
-      description: 'Mac Apple Pro Retina.',
-      price: 999999.99,
-      stock: 10,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 2,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 3,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 4,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 5,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 6,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 7,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 8,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 9,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 10,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    },
-    {
-      _id: 11,
-      description: 'Mac Apple Pro Retina2.',
-      price: 888888.88,
-      stock: 0,
-      imageUrlSm: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlMd: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-      imageUrlLg: 'https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP715/SP715-display_mbp_13.jpg',
-    }
-  ];
+  constructor () {
+  }
 
   async getAll(): Promise<Product[]> {
-    return this.products;
+    return ProductModel.find();
   }
+
 }
 
 export default ProductRepository;
